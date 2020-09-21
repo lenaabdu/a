0cd7bdf74af1788b2c5dc022340c09971916b3ba
lenaabdu <lenaabdu23@gmail.com>
2020-09-21 14:08:13 +0100

    Updates

---
+++ precache-manifest.428bd8cce0409332ad561bb3898d80b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39813fbf47a9a2c7a3d170ea359b507c",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "ab146100a86781c9c6f1",
+    "url": "/blog-app/static/css/2.ae1ec7d8.chunk.css"
+  },
+  {
+    "revision": "61ae87fe3cfaf17de46d",
+    "url": "/blog-app/static/css/main.b50d76d2.chunk.css"
+  },
+  {
+    "revision": "ab146100a86781c9c6f1",
+    "url": "/blog-app/static/js/2.966b9c1e.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/blog-app/static/js/2.966b9c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ae87fe3cfaf17de46d",
+    "url": "/blog-app/static/js/main.651482ce.chunk.js"
+  },
+  {
+    "revision": "68df45c93ca92c4acb27",
+    "url": "/blog-app/static/js/runtime-main.3d8fb2c8.js"
+  }
+]);
